ca2ee8b37fbe234353e8365c9ab3669bcd7d5fc7
Matthias Gansrigler <opensource@eternalstorms.at>
2021-08-24 16:35:29 +0200

    FIX license in cod

--- ESSAppStoreConnectAPI/ESSAppStoreConnectAPI.h
+++ ESSAppStoreConnectAPI/ESSAppStoreConnectAPI.h
@@ -5,6 +5,57 @@
 //  Created by Matthias Gansrigler on 12.02.2019.
 //  Copyright © 2019 Eternal Storms Software. All rights reserved.
 //
+
+/*
+  MIT License
+  
+  Copyright (c) 2019-present Matthias Gansrigler (Eternal Storms Software, https://eternalstorms.at)
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+  */
+
+ /*
+  API calls pieced together from fastlane ( https://github.com/fastlane/fastlane )
+  
+  The MIT License (MIT)
+  
+  Copyright (c) 2015-present the fastlane authors
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+  */
+
 #import "TargetConditionals.h"
 
 #if TARGET_OS_OSX
--- ESSAppStoreConnectAPI/ESSAppStoreConnectAPI.m
+++ ESSAppStoreConnectAPI/ESSAppStoreConnectAPI.m
@@ -6,6 +6,56 @@
 //  Copyright © 2019 Eternal Storms Software. All rights reserved.
 //
 
+/*
+  MIT License
+  
+  Copyright (c) 2019-present Matthias Gansrigler (Eternal Storms Software, https://eternalstorms.at)
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+  */
+
+ /*
+  API calls pieced together from fastlane ( https://github.com/fastlane/fastlane )
+  
+  The MIT License (MIT)
+  
+  Copyright (c) 2015-present the fastlane authors
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+  */
+
 #import "ESSAppStoreConnectAPI.h"
 
 @interface ESSAppStoreConnectAPI ()
@@ -752,7 +802,6 @@ static ESSAppStoreConnectAPI *_shAPI = nil;
 	[req setValue:@"application/json, text/javascript" forHTTPHeaderField:@"Accept"];
 	[req setValue:@"keep-alive" forHTTPHeaderField:@"Connection"];
 	[req setValue:@"PromoCodes for Mac and iOS by Eternal Storms Software" forHTTPHeaderField:@"User-Agent"];
-	[req setValue:@"itc" forHTTPHeaderField:@"X-Csrf-Itc"];
 
 	if (additionalFields.allKeys.count != 0)
 	{
